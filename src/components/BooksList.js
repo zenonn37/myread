86b47d39eb274270ab37a2786c1f0ecef3824f17
Chris <christophercrowd@gmail.com>
2021-02-05 09:30:05 -0500

    feat:Added book filtered data to all books components

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,25 +2,33 @@ import React from 'react'
 import SelectComp from './Select'
 
 
-const BookList = () => (
+const BookList = ({books,shelfHandler}) => {
+
+    const {title, authors,imageLinks,shelf} = books
+    
+     return(
   
         
                     <li>
                         <div className="book">
                             <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
                                 <div className="book-shelf-changer">
-                                <SelectComp />
+                                <SelectComp shelf={shelf} shelfHandler={shelfHandler} />
                                 </div>
                             </div>
-                            <div className="book-title">Ender's Game</div>
-                            <div className="book-authors">Orson Scott Card</div>
+                            <div className="book-title">{title}</div>
+                            <div className="book-authors">
+                                {authors.map((author) =>(
+                                    <span key={author}>{author}</span>
+                                ))}
+                            </div>
                         </div>
                     </li>
   
-  
+  );
 
 
-)
+}
 
 export default BookList
