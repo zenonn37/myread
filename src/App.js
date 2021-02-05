f616a5b93fc6499ccc897de43bc881a1ef77b811
Chris <christophercrowd@gmail.com>
2021-02-05 17:13:51 -0500

    chore:Upgraded app to React 16.8, added framer motion and react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+
+
+
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Books from './components/Books'
 import Header from './components/Header'
 import Search from './components/Search'
+import { Route } from "react-router-dom";
 
 
 const SHELF_1 = "currentlyReading";
@@ -63,24 +67,34 @@ class BooksApp extends React.Component {
     const {books} = this.state
     return (
       <div className="app">
-       
-         <Search/>
-       
-          <div>
-              <Header/>
-            <div className="list-books-content">
-              <div>
-                 <Books status={SHELF_1} books={books} shelfHandler={this.shelfHandler}/>
-                 <Books status={SHELF_2} books={books} shelfHandler={this.shelfHandler}/>
-                 <Books status={SHELF_3} books={books} shelfHandler={this.shelfHandler}/>
+         <Route path="/search" render={() => (
+
+               <Search books={books}/>
+
+         )}/>
          
-             
-              </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => {}}>Add a book</button>
+         <Route exact path="/" render={
+          () => (            
+            <div>
+            <Header/>
+          <div className="list-books-content">
+            <div>
+               <Books status={SHELF_1} books={books} shelfHandler={this.shelfHandler}/>
+               <Books status={SHELF_2} books={books} shelfHandler={this.shelfHandler}/>
+               <Books status={SHELF_3} books={books} shelfHandler={this.shelfHandler}/>
+       
+           
             </div>
           </div>
+          <div className="open-search">
+            <button onClick={() => {}}>Add a book</button>
+          </div>
+        </div>
+          )}/>
+        
+
+
+          
           </div>
         
     
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import SelectComp from './Select'
+import { motion } from "framer-motion"
 
 
 const BookList = ({books,shelfHandler}) => {
 
-    const {title, authors,imageLinks,shelf,id} = books
+    const {title, authors,imageLinks,shelf} = books
     
      return(
   
         
-                    <li>
+                    <motion.li   
+                    whileHover={{ scale: 1.2 }}>
                         <div className="book">
                             <div className="book-top">
                                 <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
@@ -24,7 +26,7 @@ const BookList = ({books,shelfHandler}) => {
                                 ))}
                             </div>
                         </div>
-                    </li>
+                    </motion.li>
   
   );
 
