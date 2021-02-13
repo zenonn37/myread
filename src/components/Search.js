c750857c27bda89dda3b5bb5ac1fd51d97647cd4
Chris <christophercrowd@gmail.com>
2021-02-13 12:09:43 -0500

    docs:Updated and added more documentation

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React,{Component} from "react";
+import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import BookList from "./BooksList";
 import * as BooksAPI from '../BooksAPI'
@@ -13,17 +13,12 @@ class Search extends React.Component {
   }
 
   
-  goBack = () =>  {
-    const history = useHistory();
-    history.push('/');
-  }
-
   
 
   searchBooks = (query) => {      
     //do not allow empty query string to server
     if (query === ""){
-      
+       //set query guard to false
       this.setState(() =>({
         search:[],
         query:false,
@@ -34,9 +29,9 @@ class Search extends React.Component {
     
     BooksAPI.search(query)
       .then((data) =>{
-     
+         //handle query errors
         if (data.error === "undefined" || data.error === "empty query") {
-            
+              //set query guard to false
               this.setState(() =>({
                 search:[],
                 query:false,
@@ -52,63 +47,47 @@ class Search extends React.Component {
         const bkShelf = this.props.books;
        
         let shelf =[];
-       
-        data.find(el1=>{
+        //find book shelf matches with search results
+        data.find(el1 => {
+
           var matched = bkShelf.find(el2 => {
               
               return el2.id === el1.id;
           })
-          if (matched != null) shelf.push(matched);         
+          if (matched != null) shelf.push(matched);   
+          //return el1;      
              
       
               
           
-      })
-
+      });
+     //adds shelf attribute to raw data temp array
       const sFinal = data.map((e) =>({
         ...e,
         shelf:'none'
     }));
-
+     //combine two shelf and updated search results 
      const appendShelf = [...shelf, ...sFinal];
 
-
+    //filter out all duplicates using Array SET
     const view = Array.from(new Set(appendShelf.map(a => a.id)))
     .map(id => {
       return appendShelf.find(a => a.id === id);
     });
+      //set new state form new array
+      //set query guard to true                               
+    this.setState(() =>({
+      search:view,
+      query:true,
+    }));
    
-    
+   
 
-      // console.log(shelf);
-      // console.log(sFinal);
-      // console.log(appendShelf);
-      // console.log(view);
-
-
-        
-       
-      
-    
-      
-  
-
-      
-         
-       
+   
+ 
+  });     
+           
      
-        
-
-        this.setState(() =>({
-          search:view,
-          query:true,
-        }));
-       
-       
-
-       
-     
-      });
      
   };
 
@@ -118,10 +97,11 @@ class Search extends React.Component {
            
     
        
-     
+     //add books to shelf from search
   BooksAPI.update(book,shelf)
    .then((data) =>{
   
+      
      const update = this.state.search.map((b) => b.id === book.id ? {...b,shelf:shelf} : b);
     
      this.setState(() =>({
