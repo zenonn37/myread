5231dd17e9b620f571278d1d6dfe536f1a202745
Chris <christophercrowd@gmail.com>
2021-02-16 09:35:56 -0500

    style:Updated CSS header, buttons and dropdowns to one primary color
    Changed alignment of logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 
 
@@ -10,13 +9,7 @@ const  Header = () => {
         <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
-          <ul>
-            <li>
-              <Link to="/search">
-                Search
-                </Link>
-            </li>
-          </ul>
+        
         </div>
         </div>
     </>
