9ac24f2a312e589299c0c63beafea64c7ceca4da
Chris <christophercrowd@gmail.com>
2021-02-18 07:42:36 -0500

    chore: build test to check for errors

---
+++ build/precache-manifest.6d07d90da31d18048543633bcd7aa9c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "631b1db72142a39288fb",
+    "url": "/static/css/main.9f498d3b.chunk.css"
+  },
+  {
+    "revision": "631b1db72142a39288fb",
+    "url": "/static/js/main.631b1db7.chunk.js"
+  },
+  {
+    "revision": "e23f062c96a652f725ec",
+    "url": "/static/js/1.e23f062c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "6f94d8589919fe7a3b672fd20e57c317",
+    "url": "/index.html"
+  }
+];
